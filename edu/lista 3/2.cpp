da7c37824b82025670386fec220af62d6e5d95cc
Wojtek Kubiak <wojtekkubiak@outlook.com>
2019-03-27 21:22:36 +0100

    Update 2.cpp

--- edu/lista 3/2.cpp
+++ edu/lista 3/2.cpp
@@ -1,7 +1,7 @@
 #include <climits>
 #include <iostream>
 
-void insertionSort(int a[], int n) {
+void insertion_sort(int a[], int n) {
     int v;
     a[-1] = INT_MIN;
 
@@ -22,7 +22,7 @@ int main() {
     int tab[] = {3, -1, 2, 5, 8, 0, -4, 10};
     int k = sizeof(tab)/sizeof(tab[0]);
 
-    insertionSort(tab, k);
+    insertion_sort(tab, k);
 
     for(auto x : tab) {
         std::cout << x << " ";
