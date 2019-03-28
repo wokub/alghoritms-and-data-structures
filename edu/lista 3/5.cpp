e833655f8911597302940654d0f9d4f620239e49
Wojtek Kubiak <wojtekkubiak@outlook.com>
2019-03-28 09:55:52 +0100

    Update 5.cpp

--- edu/lista 3/5.cpp
+++ edu/lista 3/5.cpp
@@ -1,49 +1,50 @@
 #include <iostream>
 
-void merge(int arr[], int start, int mid, int end) {
-    int start2 = mid + 1;
+void merge(int tab[], int start, int mid, int end) {
+    int tempStart = mid + 1;
 
-    if (arr[mid] <= arr[start2]) { return; }
+    while (start <= mid && tempStart <= end) {
 
-    while (start <= mid && start2 <= end) {
-
-        if (arr[start] <= arr[start2]) {
-            start++;
-        } else {
-            int value = arr[start2];
-            int index = start2;
+        if(tab[start] > tab[tempStart]) {
+            int value = tab[tempStart];
+            int index = tempStart;
 
             while (index != start) {
-                arr[index] = arr[index - 1];
+                tab[index] = tab[index - 1];
+                
                 index--;
             }
-            arr[start] = value;
+
+            tab[start] = value;
 
             start++;
             mid++;
-            start2++;
-        }
+            tempStart++;
+        } else  
+            start++;
     }
 }
 
-void mergesort(int arr[], int l, int r) {
+
+
+void mergesort(int tab[], int l, int r) {
 
     if (l < r) {
         int mid = (l + r) / 2;
-        mergeSort(arr, l, mid);
-        mergeSort(arr, mid + 1, r);
-        merge(arr, l, mid, r);
+        
+        mergesort(tab, l, mid);
+        mergesort(tab, mid + 1, r);
+        merge(tab, l, mid, r);
     }
 }
+
+
 
 int main() {
     int t[] = {9, 3, 8, 2};
 
-    int n = sizeof(t) / sizeof(t[0]);
-
-    mergesort(t, 0, n-1);
+    mergesort(t, 0, sizeof(t) / sizeof(t[0]) - 1);
 
     for(int x : t) { std::cout << x << " "; }
-
-    std::cout << std::endl;
+    
 }
