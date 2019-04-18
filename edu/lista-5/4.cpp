48aeed6c06c70ba7ef4a327352ee7a68e3042ac3
Wojtek Kubiak <282127@uwr.edu.pl>
2019-04-18 15:23:52 +0200

    Update 4.cpp

--- edu/lista-5/4.cpp
+++ edu/lista-5/4.cpp
@@ -10,17 +10,17 @@ int max(int t[], int n) {
 
 void countingSort(int t[], int n, int c) {
     int tmp[n];
-    int count[10] = {0};
+    int x[10] = {0};
 
     for (int i = 0; i < n; i++)
-        count[(t[i] / c) % 10]++;
+        x[(t[i] / c) % 10]++;
 
     for (int i = 1; i < 10; i++)
-        count[i] += count[i - 1];
+        x[i] += x[i - 1];
 
     for (int i = n - 1; i >= 0; i--) {
-        tmp[count[(t[i] / c) % 10] - 1] = t[i];
-        count[(t[i] / c) % 10]--;
+        tmp[x[(t[i] / c) % 10] - 1] = t[i];
+        x[(t[i] / c) % 10]--;
     }
 
     for (int i = 0; i < n; i++)
